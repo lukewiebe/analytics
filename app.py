06fbb42a423403555fa7c46c6af4cafdb4695655
Luke <luke@lukewiebe.com>
2022-07-07 22:00:51 -0500

    save ip and datetime of request to csv

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # app.py
 
 from flask import Flask, request
+import csv
 import time
 
 app = Flask(__name__)
@@ -9,9 +10,11 @@ app = Flask(__name__)
 
 @app.post("/")
 def log_traffic():
-	print(request.remote_addr)
-	time_stamp = time.strftime("%H:%M:%S", time.localtime())
-	print(time_stamp)
+	ip = request.remote_addr
+	time_stamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+	with open('log_file.csv', 'a') as f:
+		writer = csv.writer(f)
+		writer.writerow([time_stamp, ip])
 	return "Logged traffic", 200
 
 if __name__ == '__main__':
