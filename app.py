2e2ce4e6b4a983e51402950bbbf659a898c804fe
Luke <luke@lukewiebe.com>
2022-08-08 22:40:36 -0500

    app.py will create log.csv if none exists

--- app.py
+++ app.py
@@ -3,10 +3,12 @@
 from flask import Flask, request, jsonify
 import csv
 import time
+import os.path
 
 app = Flask(__name__)
 
 server_version = 1.0
+log_path = 'log.csv'
 
 @app.post("/")
 def log_traffic():
@@ -15,23 +17,31 @@ def log_traffic():
 	time_stamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 	client_version = request.get_json()["client_version"]
 	source_page = request.get_json()["source_page"]
+
+			
+
 	# write log entry to log.csv
 	with open('log.csv', 'a') as f:
 		fieldnames = [
-			'datetime',
-			'ip',
-			'source_page',
-			'server_version',
-			'client_version'
 			]
-		writer = csv.DictWriter(f, fieldnames=fieldnames)
-		writer.writerow({
-			'datetime': time_stamp,
-			'ip': ip,
-			'source_page': source_page,
-			'server_version': server_version,
-			'client_version': client_version
-			})
+		writer = csv.writer(f)
+
+		# if log file does not exist, create it
+		if not os.path.exists(log_path):
+			writer.writerow([
+				'datetime',
+				'ip',
+				'source_page',
+				'server_version',
+				'client_version'
+				])
+		writer.writerow([
+			time_stamp,
+			ip,
+			source_page,
+			server_version,
+			client_version
+			])
 	return "Logged traffic", 200
 
 if __name__ == '__main__':
