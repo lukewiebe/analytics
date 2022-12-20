a1c6cf1f1a7330c91b692712f8eb3f443afead29
Luke Wiebe <mail@lukewiebe.com>
2022-12-20 08:47:45 -0600

    update

--- application.py
+++ application.py
@@ -39,32 +39,5 @@ def log_traffic():
         ])
     return "Logged traffic", 200
 
-    # define contents of log entry
-    time_stamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    client_version = request.get_json()["client_version"]
-    source_page = request.get_json()["source_page"]
-
-    # write log entry to log.csv
-    with open('log.csv', 'a') as f:
-        writer = csv.writer(f)
-
-        # if log file does not exist, create it and write column names
-        if not os.path.exists(LOG_PATH):
-            writer.writerow([
-                'datetime',
-                'source_page',
-                'server_version',
-                'client_version'
-            ])
-
-        # write data to log.csv
-        writer.writerow([
-            time_stamp,
-            source_page,
-            SERVER_VERSION,
-            client_version
-        ])
-    return "Logged traffic", 200
-
 if __name__ == '__main__':
     app.run()
